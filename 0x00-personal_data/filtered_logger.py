53af137a61f0fff1dc8f66dc6b6cc955618d7c0b
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-05-25 22:41:23 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" Protecting PII """
+"""
+Protecting PII
+"""
 
 from typing import List
 import logging
-import mysql.connector
 import os
 import re
 
@@ -12,7 +13,7 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ returns the log message obfuscated """
+    """ Returns the log message obfuscated """
     temp = message
     for field in fields:
         temp = re.sub(field + "=.*?" + separator,
@@ -21,21 +22,28 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 def get_logger() -> logging.Logger:
-    """ Returns logger obj  """
-    return logging.getLogger('user_data')
+    """ Returns a logger object """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db() -> None:
     """ Connects to MySQL db using env vars """
     user = os.environ.get('PERSONAL_DATA_DB_USERNAME', None)
     password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', None)
     db_host = os.environ.get('PERSONAL_DATA_DB_HOST', None)
     db_name = os.environ.get('PERSONAL_DATA_DB_NAME', None)
 
-    return mysql.connector.connect(user=user,
-                                   password=password,
-                                   host=db_host,
-                                   database=db_name)
+    return None
 
 
 class RedactingFormatter(logging.Formatter):
@@ -46,13 +54,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ inits class instance """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        """ Initializes the class instance """
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ filters values in incoming log records """
+        """ Filters values in incoming log records """
         return filter_datum(
-            self.fields, self.REDACTION, super(
-                RedactingFormatter, self).format(record),
+            self.fields, self.REDACTION, super().format(record),
             self.SEPARATOR)
