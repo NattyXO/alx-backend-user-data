f333fc77318b33dfd47562aa6f635ff2e552bd6c
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-05-25 23:02:33 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,75 +1,103 @@
 #!/usr/bin/env python3
+""" 0x05. Personal data
+How to implement a log filter that will obfuscate PII fields
+How to encrypt a password and check the validity
+How to authenticate to a database using environment variables
 """
-Protecting PII
-"""
-
-import logging
 import os
 import re
+import mysql.connector
 from typing import List
+import logging
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Returns the log message with obfuscated PII"""
-    for field in fields:
-        pattern = re.compile(rf"{field}=.*?{separator}")
-        message = pattern.sub(f"{field}={redaction}{separator}", message)
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ a function called filter_datum that returns the log message
+        Args:
+            fields: a list of strings representing all fields
+            redaction: a string representing by what the field 
+            message: a string representing the log line
+            separator: a string representing by which character is 
+            separating all fields in the log line (message) 
+        Returns: message
+    """
+    for format in fields:
+        message = re.sub(rf"{format}=(.*?)\{separator}",
+                         f'{format}={redaction}{separator}', message)
     return message
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+        
 def get_logger() -> logging.Logger:
-    """Returns a logger object configured with required settings"""
-    logger = logging.getLogger('user_data')
+    """ a get_logger function that takes no arguments and 
+    returns a logging.Logger object
+    """
+
+    logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
-    formatter = RedactingFormatter(
-        fmt="[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s",
-        redaction="***",
-        separator=";"
-    )
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(stream_handler)
-
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
     return logger
 
 
-class RedactingFormatter(logging.Formatter):
-    """Custom log formatter to obfuscate PII"""
-
-    def __init__(self, fmt: str, redaction: str, separator: str):
-        super().__init__(fmt)
-        self.redaction = redaction
-        self.separator = separator
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Formats the log record by obfuscating PII fields"""
-        message = super().format(record)
-        return filter_datum(PII_FIELDS, self.redaction, message, self.separator)
-
-
-def get_db() -> None:
-    """Connects to MySQL db using environment variables"""
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    db_host = os.getenv('PERSONAL_DATA_DB_HOST')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-
-    if not all([user, password, db_host, db_name]):
-        raise ValueError("Incomplete or missing environment variables for database connection.")
-
-    return create_db_connection(user, password, db_host, db_name)
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ connect to a secure holberton database to read a users table.
+    """
+    DB_user = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    DB_password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    DB_host_name = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    DB_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    connection = mysql.connector.connect(
+        host=DB_host_name,
+        database=DB_name,
+        user=DB_user,
+        password=DB_password)
+    return connection
 
 
-def create_db_connection(user: str, password: str, host: str, database: str):
-    """Creates a database connection"""
-    # Replace this code with your actual implementation.
-    print(f"Creating database connection for user={user}, "
-          f"host={host}, database={database}")
-    return None
+def main():
+    """ a main function that takes no arguments and returns 
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+if __name__ == "__main__":
+    main()
